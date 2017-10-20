9d44a56fd1c6e95d836a412c13a8cfdbabe57429
Fernando Martin Perez <farrunandi@gmail.com>
2017-10-20 09:37:56 +0200

    modificado ej11 tema4

--- Tema4_Ejercicios24/Tema4Ejercicio11.java
+++ Tema4_Ejercicios24/Tema4Ejercicio11.java
@@ -8,34 +8,29 @@
 */
 public class Tema4Ejercicio11 {
     public static void main(String[] args) {
-    
+    // Meto variables fijas que me ayudaran para los calculos
     int relojHora = 24;
     int relojMinutos = 00;
-
+    
+    // Introduzco las variables hora y 
     System.out.println("¿Cuanto falta hasta medianoche?");
-    System.out.print("Introduzca la hora: ");
+    System.out.print("Introduzca la hora (entre las 00 y las 23): ");
     int hora = Integer.parseInt(System.console().readLine());
-    System.out.print("Ahora los minutos: ");
+    System.out.print("Ahora los minutos (entre las 00 y las 59): ");
     int minutos = Integer.parseInt(System.console().readLine());
     
-    int difHora = relojHora - hora;
-    int difMin = 60 - minutos;
+    if ((hora < 0) || (hora > 23) || (minutos < 0) || (minutos > 59)) {
+      System.out.print("Los datos introducidos no son correctos");
+    } else {
+    // Hago los calculos
+          int difHora = relojHora - hora;
+          int difMin = 60 - minutos;
     
-    System.out.println("Faltan " + difHora + "hora" + difMin + " minutos");
-    //System.out.println(difMin);
-    
-    System.out.print("Faltan hasta medianoche: ");
-    System.out.print((difHora *3600) + (difMin *60));
-    System.out.print(" segundos");
-    
-    
-    
-    
-    
-    
-  
-    
-    //System.out.print("Faltan hasta medianoche" + segundos + "segundos");
-
+    // Represento la solución
+          System.out.println("Faltan " + difHora + " hora " + difMin + " minutos");
+          System.out.print("Faltan hasta medianoche: ");
+          System.out.print((difHora * 3600) + (difMin * 60));
+          System.out.print(" segundos");    
+    }
   }
 }
